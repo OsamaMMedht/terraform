34b80cdb2d712143be7904e370debd0f967852b6
osama357 <osama.mohamedmedhat@gmail.com>
2025-05-06 00:25:32 +0300

    last update

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -1,4 +1,4 @@
-resource "aws_instance" "example" {
+resource "aws_instance" "mainec2module" {
   ami           = var.ami
   instance_type = var.instance_type
   key_name      = var.key_name
--- modules/ec2/output.tf
+++ modules/ec2/output.tf
@@ -1,3 +1,8 @@
 output "instance_id" {
-  value = aws_instance.example.id
+  value = aws_instance.mainec2module.id
+  
+}
+
+output "instance_name" {
+  value = aws_instance.mainec2module.tags["Name"]
 }
