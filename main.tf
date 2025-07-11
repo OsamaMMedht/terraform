6f66af85c86d8c23517d622be70b6b500a77111c
osama357 <osama.mohamedmedhat@gmail.com>
2025-07-11 17:11:15 +0300

    #add new file

--- main.tf
+++ main.tf
@@ -1,4 +1,3 @@
-#VPCs
 module "main_vpc" {
   source = "./modules/prod/vpc"
   vpc_cidr             = "172.21.0.0/16"
@@ -10,18 +9,6 @@ module "main_vpc" {
   vpc_name             = "prod-vpc"
 }
 
-module "dev_vpc" {
-  source = "./modules/dev/vpc"
-  vpc_cidr             = "10.20.0.0/16"
-  public_subnets_cidr  = ["10.20.20.0/24", "10.20.21.0/24"]
-  private_subnets_cidr = ["10.20.22.0/24", "10.20.23.0/24"]
-  availability_zones   = ["us-east-1c", "us-east-1d"]
-  enable_public_ip     = "true"
-  enable_dns           = "true"
-  vpc_name             = "dev-vpc"
-}
-
-#S3
 module "public_bucket" {
     source = "./modules/prod/s3"
     bucket = "public_bucket-testos"
